020b85210f2ce6082d5f0ed8413049e3e582385f
Unknown <henriquespencer_@hotmail.com>
2017-10-31 13:52:11 -0300

    esta so enviando

--- clienteWEB.py
+++ clienteWEB.py
@@ -1,40 +1,49 @@
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.widgets import TextArea
 from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
 import paho.mqtt.client as mqtt
 import sys
 
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisupposedtobesecret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+Bootstrap(app)
+db = SQLAlchemy(app) # obj de acesso ao banco
+
 reload(sys)  
 sys.setdefaultencoding('utf8')
-app = Flask(__name__)
-Bootstrap(app)
-QTT_ADDRESS = 'iot.eclipse.org'
+
+MQTT_ADDRESS = 'iot.eclipse.org'
 MQTT_PORT = 1883
 MQTT_TIMEOUT = 60
 
 class Mensagem(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    topico =db.Column(db.String(200))
-    conteudo= db.Column(db.String(200))
+	id = db.Column(db.Integer, primary_key=True)
+	topico =db.Column(db.String(200))
+	conteudo= db.Column(db.String(200))
+db.create_all()
 
 class RegisterMensagem(FlaskForm):
-    topico = StringField('topico', validators=[InputRequired(), Length(min=4, max =200)])
-    conteudo = StringField('conteudo', validators=[InputRequired(), Length(min=4, max =200)])
+	topico = StringField('topico', validators=[InputRequired(), Length(min=4, max =200)])
+	conteudo = StringField('conteudo', validators=[InputRequired(), Length(min=4, max =200)])
 
 
 
 if sys.version_info[0] == 3:
-    input_func = input
+	input_func = input
 else:
-    input_func = raw_input
+	input_func = raw_input
 
 def on_connect(client, userdata, flags, rc):
  #   print('Conectado. Resultado: %s' % str(rc))
-    result, mid = client.subscribe('/buteco/topico')
+	result, mid = client.subscribe('/buteco/topico')
  #   print('Inscrevendo-se no tópico "/buteco/topico" (%d)' % mid)
 
 
@@ -43,46 +52,50 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    if msg.topic == '/buteco/topico':
-        new_mensagem = Mensagem(conteudo= msg.payload)
-        #db.session.add(new_Comentario)
-        #db.session.commit()
-    #else:
-      #  print('Tópico desconhecido.')
+	if msg.topic == '/buteco/topico':
+		new_mensagem = Mensagem(conteudo= msg.payload, topico=msg.topic)
+		#db.session.add(new_Comentario)
+		#db.session.commit()
+	#else:
+	  #  print('Tópico desconhecido.')
 
 
 def loop():
-    client = mqtt.Client()
-    client.on_connect = on_connect
-    #client.on_subscribe = on_subscribe
-    client.on_message = on_message
-    # descomente esta linha caso seu servidor possua autenticação.
-    # client.username_pw_set(MQTT_AUTH.user, MQTT_AUTH.pwd)
-    client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
-    client.loop_forever()
+	client = mqtt.Client()
+	client.on_connect = on_connect
+	#client.on_subscribe = on_subscribe
+	client.on_message = on_message
+	# descomente esta linha caso seu servidor possua autenticação.
+	# client.username_pw_set(MQTT_AUTH.user, MQTT_AUTH.pwd)
+	client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
+	#client.loop_forever()
 
 
 def send_message(msg):
-    client = mqtt.Client()
-    # descomente esta linha caso seu servidor possua autenticação.
-    # client.username_pw_set(MQTT_AUTH.user, MQTT_AUTH.pwd)
-    client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
-    result, mid = client.publish('/buteco/topico', msg)
+	client = mqtt.Client()
+	# descomente esta linha caso seu servidor possua autenticação.
+	# client.username_pw_set(MQTT_AUTH.user, MQTT_AUTH.pwd)
+	client.connect(MQTT_ADDRESS, MQTT_PORT, MQTT_TIMEOUT)
+	result, mid = client.publish('/buteco/topico', msg)
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
 	loop()
-    mensagem = Mensagem.query.all()
+	mensagem = Mensagem.query.all()
+	form = RegisterMensagem()
+	
+	test='fbdn'
 
-    if form.validate_on_submit():
-        send_message(msg)
-        #new_Comentario = Comentario( idpost=id, comentario=form.comentario.data )
-        #new_user = User(username=form.username.data, email=form.username.data, password=form.password.data)
-        #db.session.add(new_Comentario)
-        #db.session.commit()
-        
-	return render_template('index.html', form=form)
+	if form.validate_on_submit():
+	    send_message(form.conteudo.data)
+		#new_Comentario = Comentario( idpost=id, comentario=form.comentario.data )
+		#new_user = User(username=form.username.data, email=form.username.data, password=form.password.data)
+		#db.session.add(new_Comentario)
+		#db.session.commit()
+		
+	return render_template('index.html', form=form, mensagem=mensagem)    
+
 
 
 
